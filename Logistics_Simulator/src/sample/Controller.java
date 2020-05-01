243ee5b7883dfd386d8c6415384b97cf2635af8f
Robert Kristian Alm <exarchiasghost@gmail.com>
2020-05-01 02:36:26 +0200

    implment intializable interface as example
    All the scenes should implement initializable interface.

--- Logistics_Simulator/src/sample/Controller.java
+++ Logistics_Simulator/src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.Initializable;
+
+import java.io.Serializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    //Robert: This will be the very first page. We will use it as a login page
+    //but let's leave it with a generic name, just to highlight that it will be the very first page.
+
+    //All the scenes should implement the Initializable interface
+    //The code inside initialize is the one that runs when the scene is loaded.
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //some code that starts in the loading of the scene
+    }
+
+    
+
 }
