21204486c12a9a3a607cf496c0325c24a459e9d4
Robert Kristian Alm <exarchiasghost@gmail.com>
2020-05-01 07:33:50 +0200

    Adding buttons on Dashboard
    Adding the buttons from the dashboard to the scenes of part1

--- Logistics_Simulator/src/sample/DashBoardController.java
+++ Logistics_Simulator/src/sample/DashBoardController.java
@@ -12,6 +12,18 @@ public class DashBoardController implements Initializable {
     @FXML
     Button logoutBtn;
 
+    @FXML
+    Button createPersonBtn;
+
+    @FXML
+    Button editPersonBtn;
+
+    @FXML
+    Button createProductBtn;
+
+    @FXML
+    Button editProductBtn;
+
     //All the scenes should implement the Initializable interface
     //The code inside initialize is the one that runs when the scene is loaded.
     @Override
@@ -25,4 +37,30 @@ public class DashBoardController implements Initializable {
     public void clickedOnLogout() throws Exception {
         RTools.logout();
     }
+
+    @FXML
+    public void clickedCreatePerson() throws Exception {
+        RTools.goToScene("createPerson");
+    }
+
+    @FXML
+    public void clickedEditPerson() throws Exception {
+        RTools.goToScene("editPerson");
+    }
+
+    @FXML
+    public void clickedCreateProduct() throws Exception {
+        RTools.goToScene("createProduct");
+    }
+
+    @FXML
+    public void clickedEditProduct() throws Exception {
+        RTools.goToScene("editProduct");
+    }
+
+
+
+
+
+
 }
