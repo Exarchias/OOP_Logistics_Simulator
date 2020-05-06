2e5ab2ea0571efb49bf0ceab59c27d7b27052172
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2020-05-06 12:40:37 +0200

    basic functionallity added to the createSimulation scene and controller

--- Logistics_Simulator/src/sample/CreateSimulationController.java
+++ Logistics_Simulator/src/sample/CreateSimulationController.java
@@ -1,4 +1,44 @@
 package sample;
 
-public class CreateSimulationController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreateSimulationController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        RTools.kickIfNotLoggedIn();
+        System.out.println("You are in the create simulation scene");
+
+    }
+
+    @FXML
+    private TextField txtTitle;
+
+    @FXML
+    private Button btnSubmit;
+
+    @FXML
+    private Button btngotodashboard;
+
+    public void clickedOnSubmit(){
+        String title = txtTitle.getText();
+        if (!title.equalsIgnoreCase("")){
+            RTools.createASimualtion(title);
+        }else {
+            RTools.createASimualtion("Default Simulation");
+        }
+
+
+    }
+
+    public void clickedOnToDashboard() throws Exception {
+        RTools.goToDashBoard();
+    }
 }
+
+
