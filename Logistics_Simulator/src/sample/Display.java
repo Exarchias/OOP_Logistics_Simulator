fb4291c2981161685ea9d3593eb29067ed4fed5e
Robert Kristian Alm <exarchiasghost@gmail.com>
2020-05-14 02:45:28 +0200

    adjusting the JDK
    small JDK issue.

--- Logistics_Simulator/src/sample/Display.java
+++ Logistics_Simulator/src/sample/Display.java
@@ -4,15 +4,6 @@ import java.util.ArrayList;
 
 public class Display {
 
-    //The main architecture for every method
-
-    public static String nameOfMethod(Object objectsName){
-        String msg= "";
-        //do some code with the object objectsName
-        return msg;
-    }
-
-
 
 
     //For displaying the details of a person
@@ -23,14 +14,79 @@ public class Display {
     }
 
 
+    //For displaying the details of a product
+    public static String displayProduct(Product product){
+        String msg= "";
+        msg = "Title: " + product.getTitle();
+        return msg;
+    }
+
+    //For displaying the details of a simulation
+    public static String displaySimulation(Simulation simulation){
+        String msg= "";
+        msg = "Title: " + simulation.getTitle();
+        return msg;
+    }
+
+    //For displaying the details of a output
+    public static String displayOutput(Output output){
+        String msg= "";
+        msg = "Title: " + output.getTitle();
+        return msg;
+    }
 
 
+
+
+    //displays the contents of an ArrayList of products
     public static String displayProductsList(ArrayList<Product> productsList){
         String msg= "";
         int count = 0;
-        msg = msg + "Products of the person \n";
-        msg = msg + "====================== \n";
+        msg = msg + "Products \n";
+        msg = msg + "======== \n";
         for(Product x : productsList){
+            count++;
+            msg = msg + "" + count + ")" + x.getTitle() + "\n";
+        }
+        return msg;
+    }
+
+
+    //displays the contents of an ArrayList of persons
+    public static String displayPersonsList(ArrayList<Person> personsList){
+        String msg= "";
+        int count = 0;
+        msg = msg + "Persons \n";
+        msg = msg + "======= \n";
+        for(Person x : personsList){
+            count++;
+            msg = msg + "" + count + ")" + x.getName() + "\n";
+        }
+        return msg;
+    }
+
+
+    //displays the contents of an ArrayList of simulations
+    public static String displaySimulationsList(ArrayList<Simulation> simulationsList){
+        String msg= "";
+        int count = 0;
+        msg = msg + "Simulations \n";
+        msg = msg + "=========== \n";
+        for(Simulation x : simulationsList){
+            count++;
+            msg = msg + "" + count + ")" + x.getTitle() + "\n";
+        }
+        return msg;
+    }
+
+
+    //displays the contents of an ArrayList of outputs
+    public static String displayOutputsList(ArrayList<Output> outputsList){
+        String msg= "";
+        int count = 0;
+        msg = msg + "Products \n";
+        msg = msg + "======== \n";
+        for(Output x : outputsList){
             count++;
             msg = msg + "" + count + ")" + x.getTitle() + "\n";
         }
